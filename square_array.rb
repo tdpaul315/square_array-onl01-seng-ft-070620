3a71b9051abb81229bc59914a0468b64bf5b2582
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-14 03:25:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ number = ["1", "2", "3"]
 def square_array(number)
   new_number = []
   number.each do |number|
-    new_number << (number)
+    new_number << (number ** 2)
+    end
     
-  end
 end
